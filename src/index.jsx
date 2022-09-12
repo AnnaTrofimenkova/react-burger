edf120f65090009276b47773b1d37bd6076959fc
Anna Trofimenkova <annatrofimenkova@yandex.ru>
2022-09-12 22:44:00 +0300

    redub

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/App";
+import { compose, createStore } from "redux";
+
+//redux
+import { Provider } from "react-redux";
+import rootReducer from "./services/reducers/index";
+import initialState from "./store/initialState";
+
+const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/store/initialState.js
@@ -0,0 +1,14 @@
+const initialState = {
+  ingredients: [
+    {
+      id: 123,
+      title: "test",
+    },
+  ],
+
+  burgerIngredients: [{}],
+  currentIngredient: {},
+  oder: {},
+};
+
+export default initialState;
